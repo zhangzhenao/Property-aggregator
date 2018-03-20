d1a3ec8018203161c08d04553c0d2c52c8be7183
mr.zhang <768532106@qq.com>
2018-03-20 08:33:48 +0800

    添加了css

---
+++ Property-dao/src/test/java/org/property/test/testGenerter.java
@@ -0,0 +1,22 @@
+package org.property.test;
+
+
+import org.junit.Test;
+
+public class testGenerter {
+
+	@Test
+	public void test() throws Exception {
+		/**
+		 * 调用方法，说明你要创建的是哪个类，可以填写多个
+		 */
+		//ProjectUtils.setClasses(new Class[] {Permission.class});
+		
+		/**
+		 * 生成dao和mapper
+		 */
+		//ProjectUtils.generateDaoInterface();
+		//ProjectUtils.generateMappers();
+	}
+
+}
